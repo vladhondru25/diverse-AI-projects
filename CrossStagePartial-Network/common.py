16df3c7521f876770295fa9b871f1d8080d2a64a
vladhondru25 <vlad.hondru25@yahoo.com>
2020-12-27 22:16:42 +0200

    Implemented CSP layers

--- CrossStagePartial-Network/common.py
+++ CrossStagePartial-Network/common.py
@@ -5,13 +5,23 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
+class Mish(nn.Module):
+    def __init__(self):
+        super(Mish,self).__init__()
+
+    def forward(self, x):
+        return x * nn.tanh(nn.softplus(x))
+
+
 ACTIVATIONS = {
+    'mish': Mish(),
     'relu': nn.ReLU(inplace=True),
     'leakyRelu': nn.LeakyReLU(negative_slope=0.01, inplace=True),
     'tanh': nn.Tanh(),
     'sigmoid': nn.Sigmoid(),
     'identity': nn.Identity()
 }
+
 
 class Conv2dBlock(nn.Module):
     def __init__(self, in_C, out_C, k, s, p, bias=False, activation='relu'):
@@ -27,10 +37,10 @@ class Conv2dBlock(nn.Module):
         return self.conv_layer(x)
 
 
-class ResidualBlock(nn.Module):
+class BottleneckBlock(nn.Module):
     def __init__(self, in_C, activation='relu'):
-        super(ResidualBlock, self).__init__()
-        
+        super(BottleneckBlock, self).__init__()
+
         self.conv1 = Conv2dBlock(in_C=in_C,    out_C=in_C//2, k=1, s=1, p=0)
         self.conv2 = Conv2dBlock(in_C=in_C//2, out_C=in_C,    k=3, s=1, p=1, activation='identity')
 
@@ -42,14 +52,6 @@ class ResidualBlock(nn.Module):
         return self.activation(z2)
 
 
-class BottleneckBlock(nn.Module):
-    def __init__(self):
-        super(BottleneckBlock, self).__init__()
-
-    def forward(self, x):
-        return x
-
-
 class CSPBlock(nn.Module):
     def __init__(self, in_C, no_of_blocks=1, activation='leakyRelu'):
         super(CSPBlock, self).__init__()
@@ -59,7 +61,7 @@ class CSPBlock(nn.Module):
 
         self.right_block = nn.Sequential(
             Conv2dBlock(in_C=in_C, out_C=hidden_channels, k=1, s=1, p=0, activation=activation),
-            *[BottleneckBlock() for _ in range(no_of_blocks)],
+            *[BottleneckBlock(hidden_channels) for _ in range(no_of_blocks)],
             nn.Conv2d(in_channels=hidden_channels, out_channels=hidden_channels, kernel_size=1, stride=1, padding=0, bias=False)
         )
 
