449166405a1027d67e7c7081235a650e91c93b0e
vladhondru25 <vlad.hondru25@yahoo.com>
2020-12-31 11:46:10 +0200

    Implemented CSPDarknet-53 architecture

---
+++ YOLO-object-detection/models/CSPdarknet-53.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from common import Conv2dBlock, CSPBlock
+
+
+class CSPDarknet53(nn.Module):
+    def __init__(self):
+        super(CSPDarknet53, self).__init__()
+
+        self.downsample_layers = nn.ModuleList([
+            Conv2dBlock(in_C=32,  out_C=64,   k=3, s=2, p=1),
+            Conv2dBlock(in_C=64,  out_C=128,  k=3, s=2, p=1),
+            Conv2dBlock(in_C=128, out_C=256,  k=3, s=2, p=1),
+            Conv2dBlock(in_C=256, out_C=512,  k=3, s=2, p=1),
+            Conv2dBlock(in_C=512, out_C=1024, k=3, s=2, p=1)
+        ])
+
+        self.csp_blocks = nn.ModuleList([
+            CSPBlock(64,   no_of_blocks=1, activation='leakyRelu'),
+            CSPBlock(128,  no_of_blocks=2, activation='leakyRelu'),
+            CSPBlock(256,  no_of_blocks=8, activation='leakyRelu'),
+            CSPBlock(512,  no_of_blocks=8, activation='leakyRelu'),
+            CSPBlock(1024, no_of_blocks=4, activation='leakyRelu'),
+        ])
+
+        self.network = nn.Sequential(
+            # Initial low-level features
+            Conv2dBlock(in_C=3,  out_C=32, k=3, s=1, p=1),
+            # CSP Blocks and downsample layers
+            *[val for pair in zip(self.downsample_layers, self.csp_blocks) for val in pair],
+            # Global average pooling
+            nn.AvgPool2d(kernel_size=8),
+            # Flatten
+            nn.Flatten(),
+            # Fully-connected layer
+            nn.Linear(in_features=1024, out_features=1000)
+        )
+
+    def forward(self, x):
+        return self.network(x)
+
+
+
+if __name__ == "__main__":
+    modelTest = CSPDarknet53()
+    xTest = torch.rand((32,3,256,256))
+    print(modelTest(xTest).shape)
--- YOLO-object-detection/models/common.py
+++ YOLO-object-detection/models/common.py
@@ -5,7 +5,16 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
+class Mish(nn.Module):
+    def __init__(self):
+        super(Mish,self).__init__()
+
+    def forward(self, x):
+        return x * nn.tanh(nn.softplus(x))
+
+
 ACTIVATIONS = {
+    'mish': Mish(),
     'relu': nn.ReLU(inplace=True),
     'leakyRelu': nn.LeakyReLU(negative_slope=0.01, inplace=True),
     'tanh': nn.Tanh(),
@@ -13,9 +22,10 @@ ACTIVATIONS = {
     'identity': nn.Identity()
 }
 
+
 class Conv2dBlock(nn.Module):
-    def __init__(self, in_C, out_C, k, s, p, bias=False, activation='relu'):
-        super(Conv2dBlock,self).__init__()
+    def __init__(self, in_C, out_C, k, s, p, bias=False, activation='leakyRelu'):
+        super(Conv2dBlock, self).__init__()
 
         self.conv_layer = nn.Sequential(
             nn.Conv2d(in_channels=in_C, out_channels=out_C, kernel_size=k, stride=s, padding=p, bias=bias),
@@ -26,10 +36,11 @@ class Conv2dBlock(nn.Module):
     def forward(self, x):
         return self.conv_layer(x)
 
-class ResidualBlock(nn.Module):
-    def __init__(self, in_C, activation='relu'):
-        super(ResidualBlock,self).__init__()
-        
+
+class BottleneckBlock(nn.Module):
+    def __init__(self, in_C, activation='leakyRelu'):
+        super(BottleneckBlock, self).__init__()
+
         self.conv1 = Conv2dBlock(in_C=in_C,    out_C=in_C//2, k=1, s=1, p=0)
         self.conv2 = Conv2dBlock(in_C=in_C//2, out_C=in_C,    k=3, s=1, p=1, activation='identity')
 
@@ -39,3 +50,29 @@ class ResidualBlock(nn.Module):
         z1 = self.conv1(x)
         z2 = x + self.conv2(z1)
         return self.activation(z2)
+
+
+class CSPBlock(nn.Module):
+    def __init__(self, in_C, no_of_blocks=1, activation='leakyRelu'):
+        super(CSPBlock, self).__init__()
+        hidden_channels = in_C//2
+
+        self.conv_left  = nn.Conv2d(in_channels=in_C, out_channels=hidden_channels, kernel_size=1, stride=1, padding=0, bias=False)
+
+        self.right_block = nn.Sequential(
+            Conv2dBlock(in_C=in_C, out_C=hidden_channels, k=1, s=1, p=0, activation=activation),
+            *[BottleneckBlock(hidden_channels) for _ in range(no_of_blocks)],
+            nn.Conv2d(in_channels=hidden_channels, out_channels=hidden_channels, kernel_size=1, stride=1, padding=0, bias=False)
+        )
+
+        self.common_conv = nn.Sequential(
+            nn.BatchNorm2d(in_C),
+            ACTIVATIONS[activation],
+            Conv2dBlock(in_C=in_C, out_C=in_C, k=1, s=1, p=0, activation=activation)
+        )
+
+    def forward(self, x):
+        left  = self.conv_left(x)
+        right = self.right_block(x)
+
+        return self.common_conv(torch.cat([left,right], dim=1))
